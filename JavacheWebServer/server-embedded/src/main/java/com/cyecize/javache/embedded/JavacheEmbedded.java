229f8ba1fa449786abc48b06fba09b8db88bad4c
Tsvetelin Yakimov <ceci2205@abv.bg>
2018-12-09 23:23:48 +0200

    Created new project - Javache embedded

---
+++ JavacheWebServer/server-embedded/src/main/java/com/cyecize/javache/embedded/JavacheEmbedded.java
@@ -0,0 +1,10 @@
+package com.cyecize.javache.embedded;
+
+public class JavacheEmbedded {
+
+    public static void startServer(int port) {
+        //TODO create application
+        System.out.println("");
+
+    }
+}

ec866de3b96b03b33b1ea49699c21ad39053ba9d
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-01-14 22:47:14 +0200

    Updated Solet Api. Added names of SoletConfig properties as constants.

--- SoletApi/src/main/java/com/cyecize/solet/SoletConstants.java
+++ SoletApi/src/main/java/com/cyecize/solet/SoletConstants.java
@@ -1,7 +1,19 @@
 package com.cyecize.solet;
 
+/**
+ * Contains names of attributes inside {@link SoletConfig} which are being added from other
+ * applications.
+ */
 public class SoletConstants {
     public static final String SOLET_CONFIG_ASSETS_DIR = "solet.cfg.assets.dir";
 
+    public static final String SOLET_CFG_WORKING_DIR = "solet.cfg.working.dir";
+
     public static final String SOLET_CONFIG_APP_NAME_PREFIX = "solet.cfg.app.name.prefix";
+
+    public static final String SOLET_CONFIG_SESSION_STORAGE_KEY = "solet.cfg.session.storage";
+
+    public static final String SOLET_CONFIG_SERVER_CONFIG_SERVICE_KEY = "solet.cfg.javache.config";
+
+    public static final String SOLET_CONFIG_DEPENDENCY_CONTAINER_KEY = "solet.cfg.javache.dependency.container";
 }

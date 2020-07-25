b1d38ea9e20e36eb232b94f9f89a67efbfeaca9a
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-07-25 20:12:22 +0300

    Add embedded server to deploy script. Add script for build and deploy

---
+++ automated-build-scripts/build-and-deploy.bat
@@ -0,0 +1,4 @@
+call %~dp0rebuild-dependencies.bat
+call %~dp0deploy.bat
+
+cd %~dp0
--- automated-build-scripts/deploy.bat
+++ automated-build-scripts/deploy.bat
@@ -1,7 +1,10 @@
-cd ../
+echo Starting deployment!
+
+cd %~dp0../
 rmdir /Q /S build-output
 mkdir build-output
 
 call %~dp0subtasks/deploy-javache-standard.bat
+call %~dp0subtasks/deploy-javache-embedded.bat
 
 cd %~dp0

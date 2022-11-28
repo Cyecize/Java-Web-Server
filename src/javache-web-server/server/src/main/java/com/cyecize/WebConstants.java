ec9d8bc0093e2cafdaf8d6045663d891af58e53d
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-11-29 00:48:27 +0200

    Use URI and Path to fix working dir for good on Linux and Windows. Tested on Windows: Standard, Embedded IDE, Embedded (java -jar) and Linux: Standard, Embedded (java -jar), Embedded under Docker.

--- src/javache-web-server/server/src/main/java/com/cyecize/WebConstants.java
+++ src/javache-web-server/server/src/main/java/com/cyecize/WebConstants.java
@@ -2,24 +2,29 @@ package com.cyecize;
 
 import com.cyecize.ioc.config.MagicConfiguration;
 import com.cyecize.javache.api.JavacheComponent;
+import com.cyecize.javache.common.PathUtils;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 
 public final class WebConstants {
-
-    private final static String START_UP_PACKAGE_PATH = StartUp.class.getName()
-            .replace(StartUp.class.getSimpleName(), "")
-            .replaceAll("\\.", "/");
 
     public static final int DEFAULT_SERVER_PORT = 8000;
 
     public static final int JAVACHE_CONFIG_EMPTY_PORT = -1;
 
-    public static final String WORKING_DIRECTORY = URLDecoder.decode(StartUp.class.getResource("").toString()
-            .replace("file:/", "/")
-            .replace(START_UP_PACKAGE_PATH, ""), StandardCharsets.UTF_8)
-            + "../";
+    public static final String WORKING_DIRECTORY;
+
+    static {
+        try {
+            final URI uri = StartUp.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+            final Path path = Path.of(PathUtils.appendPath(Path.of(uri).toString(), "../")).normalize();
+            WORKING_DIRECTORY = PathUtils.appendPath(path.toString(), "");
+        } catch (URISyntaxException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
     public static final MagicConfiguration JAVACHE_IOC_CONFIGURATION = new MagicConfiguration()
             .scanning().addCustomServiceAnnotation(JavacheComponent.class)
